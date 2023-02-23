a8f3da93be064765f44cff15b57807973956bef2
Malte Buschmann <buschman@n0000.scs00>
2023-02-23 12:15:59 -0800

    Add empty project template

---
+++ Projects/AxionStrings/AxionStrings.cpp
@@ -0,0 +1,13 @@
+#include <AxionStrings.H>
+
+// constructor
+//
+AxionStrings::AxionStrings ()
+{
+	amrex::Print() << "Starting AxionStrings project..." << std::endl;
+}
+
+// deconstructor
+//
+AxionStrings::~AxionStrings ()
+{}
