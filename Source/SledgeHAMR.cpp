3f1cff7df947685b27297a792539a025b9b5522c
MSABuschman <buschman@umich.edu>
2023-02-24 11:59:57 -0800

    Add LevelSynchronizer and implement MakeNewLevelFromCoarse.

--- Source/SledgeHAMR.cpp
+++ Source/SledgeHAMR.cpp
@@ -7,3 +7,20 @@ SledgeHAMR::SledgeHAMR ()
 
 SledgeHAMR::~SledgeHAMR ()
 {}
+
+void SledgeHAMR::MakeNewLevelFromCoarse (int lev, amrex::Real time, const amrex::BoxArray& ba,
+					     const amrex::DistributionMapping& dm)
+{
+	// Define a new level from scratch
+	const int ncomp = grid_new[lev-1].nComp();
+	const int nghost = grid_new[lev-1].nGrow();
+
+	grid_new[lev].define(ba, dm, ncomp, nghost);
+	grid_old[lev].define(ba, dm, ncomp, nghost);
+
+	grid_new[lev].t = time;
+	grid_old[lev].t = time - 1.e200;
+
+	// Fill new level with coarse level data
+	level_synchronizer.FillCoarsePatch(lev, time, grid_new[lev]);
+}
