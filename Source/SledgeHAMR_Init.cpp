4426c58759896551e604819032115dc12dc152ad
MSABuschman <buschman@umich.edu>
2023-03-02 17:28:29 -0800

    Replace prob_hi and prob_lo with L

--- Source/SledgeHAMR_Init.cpp
+++ Source/SledgeHAMR_Init.cpp
@@ -38,20 +38,29 @@ void SledgeHAMR_Init::FinishAMReXSetup ()
 	int grid_size = 0;
 	bool shadow_hierarchy = false;
 
-	amrex::ParmParse pp("amr");
-	pp.get("coarse_level_grid_size", grid_size);
-	pp.get("shadow_hierarchy", shadow_hierarchy);
+	amrex::ParmParse pp_amr("amr");
+	pp_amr.get("coarse_level_grid_size", grid_size);
+	pp_amr.get("shadow_hierarchy", shadow_hierarchy);
 
 	if( shadow_hierarchy )
 		grid_size /= 2;
 
-	std::vector<int> vect(3,grid_size);
-	pp.addarr("n_cell", vect);
+	std::vector<int> grid_vect(3,grid_size);
+	pp_amr.addarr("n_cell", grid_vect);
 
 	// Determine maximum number of levels.
 	int max_refinement_levels = 0;
-	pp.get("max_refinement_levels", max_refinement_levels);
+	pp_amr.get("max_refinement_levels", max_refinement_levels);
 
 	int max_level = max_refinement_levels + shadow_hierarchy;
-	pp.add("max_level", max_level);
+	pp_amr.add("max_level", max_level);
+
+	// Set box dimensions
+	amrex::ParmParse pp_sim("sim");
+	double L;
+	pp_sim.get("L", L);
+	std::vector<double> prob_lo(3,0);
+	std::vector<double> prob_hi(3,L);
+	pp_amr.addarr("geometry.prob_lo", prob_lo);
+	pp_amr.addarr("geometry.prob_hi", prob_hi);
 }
