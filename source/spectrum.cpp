6e970e08a44a273dceac3521a0577280122ca150
MSABuschman <buschman@umich.edu>
2023-06-13 14:56:41 -0700

    Replace Fft in spectrum calculation with dedicated function

--- source/spectrum.cpp
+++ source/spectrum.cpp
@@ -41,73 +41,7 @@ void Spectrum::Compute(const int id, const hid_t file_id, Sledgehamr* sim) {
         }
     }
 
-    int nx = ba[0].size()[0];
-    int ny = ba[0].size()[1];
-    int nz = ba[0].size()[2];
-
-    amrex::Box domain(sim->geom[lev].Domain());
-    int nbx = domain.length(0) / nx;
-    int nby = domain.length(1) / ny;
-    int nbz = domain.length(2) / nz;
-    int nboxes = nbx * nby * nbz;
-
-    amrex::Vector<int> rank_mapping;
-    rank_mapping.resize(nboxes);
-
-    for (int ib = 0; ib < nboxes; ++ib) {
-        int i = ba[ib].smallEnd(0) / nx;
-        int j = ba[ib].smallEnd(1) / ny;
-        int k = ba[ib].smallEnd(2) / nz;
-
-        int local_index = k*nbx*nby + j*nbx + i;
-        rank_mapping[local_index] = sim->dmap[lev][ib];
-    }
-
-    // Assuming nx = ny = nz
-    int Ndims[3] = { nbz, nby, nbx };
-    int     n[3] = { domain.length(2), domain.length(1), domain.length(0)};
-    hacc::Distribution d(MPI_COMM_WORLD, n, Ndims, &rank_mapping[0]);
-    hacc::Dfft dfft(d);
-
-    for (amrex::MFIter mfi(field, false); mfi.isValid(); ++mfi) {
-        int gid = mfi.index();
-        size_t local_size  = dfft.local_size();
-
-        std::vector<complex_t, hacc::AlignedAllocator<complex_t, ALIGN> > a;
-        std::vector<complex_t, hacc::AlignedAllocator<complex_t, ALIGN> > b;
-
-        a.resize(nx*ny*nz);
-        b.resize(nx*ny*nz);
-
-        dfft.makePlans(&a[0],&b[0],&a[0],&b[0]);
-
-        size_t local_indx = 0;
-        for(size_t k = 0; k < (size_t)nz; k++) {
-            for(size_t j = 0; j < (size_t)ny; j++) {
-                for(size_t i = 0; i < (size_t)nx; i++) {
-                    complex_t temp(field[mfi].dataPtr()[local_indx], 0.);
-                    a[local_indx] = temp;
-                    local_indx++;
-                }
-            }
-        }
-
-        dfft.forward(&a[0]);
-        d.redistribute_2_to_3(&a[0], &b[0] ,2);
-
-        size_t global_size  = dfft.global_size();
-
-        local_indx = 0;
-        for(size_t k = 0; k < (size_t)nz; k++) {
-            for(size_t j = 0; j < (size_t)ny; j++) {
-                for(size_t i = 0; i < (size_t)nx; i++) {
-                    field_fft[mfi].dataPtr()[local_indx] =
-                            std::abs(b[local_indx]);
-                    local_indx++;
-                }
-            }
-        }
-    }
+    Fft(field, 0, field_fft, field_fft, sim->geom[lev], true);
 
     double fac = pow(1./dimN, 6);
     double dk = 2.*M_PI / sim->L;
