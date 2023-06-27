09e91d6077f5db9d974d688889850257d0b56109
MSABuschman <buschman@umich.edu>
2023-06-27 10:36:35 -0700

    Read coarse box output in python

--- pySledgehamr/Output.py
+++ pySledgehamr/Output.py
@@ -8,6 +8,7 @@ class Output:
     def __init__(self, output_folder):
         self._prefix = output_folder
         self._slice_headers = []
+        self._coarse_box_headers = []
 
         self.__ParseFolderStructure()
 
@@ -15,13 +16,17 @@ class Output:
     def GetTimesOfSlices(self):
         return self._slice_headers[:,0]
 
+    ## Returns array of times at which slices have been written.
+    def GetTimesOfCoarseBoxes(self):
+        return self._coarse_box_headers[:,0]
+
     ## Returns a slice.
-    # @param	i		Number of slice to be read.
-    # @param	direction	Direction of slice, e.g. 'x'.
-    # @param	level		Which level should be returned.
-    # @param	fields		List of scalar field names.
-    # @return	d		Dictionary containing the time and slices.
-    def GetSlices(self, i, direction, level, fields):
+    # @param    i           Number of slice to be read.
+    # @param    direction   Direction of slice, e.g. 'x'.
+    # @param    level       Which level should be returned.
+    # @param    fields      List of scalar field names.
+    # @return   d           Dictionary containing the time and slices.
+    def GetSlice(self, i, direction, level, fields):
         # Get relevant parameters from header
         t = self._slice_headers[i,0]
         ranks = int(self._slice_headers[i,1])
@@ -29,7 +34,7 @@ class Output:
         dim0 = int(self._slice_headers[i,3])
 
         dim = dim0 * 2**level
-        folder = self._prefix + '/slices'
+        folder = self._prefix + '/slices/'
 
         # Start dictionary
         d = dict();
@@ -39,9 +44,8 @@ class Output:
         for s in fields:
             field = np.zeros((dim,dim), dtype=np.float32)
             for f in range(ranks):
-                file = folder + '/' + str(i)\
-                    + '/Level_'+str(level)\
-                    + '/' + str(f) + '.hdf5'
+                file = folder + str(i) + '/Level_'+str(level)\
+                     + '/' + str(f) + '.hdf5'
 
                 fin = h5py.File(file,'r')
                 if 'le1_'+direction in fin.keys():
@@ -53,38 +57,106 @@ class Output:
                     for b in range(len(le1)):
                         dset = s+'_'+direction+'_'+str(b+1)
                         field[le1[b]:he1[b],le2[b]:he2[b]] =\
-                            (fin[dset][:]).reshape((he1[b]-le1[b], he2[b]-le2[b]))
+                                (fin[dset][:]).reshape(\
+                                        (he1[b]-le1[b], he2[b]-le2[b]))
 
             # Add result to dict.
             d[s] = field
 
         return d
 
+    ## Returns a coarse box.
+    # @param    i           Number of slice to be read.
+    # @param    fields      List of scalar field names.
+    # @return   d           Dictionary containing the time and slices.
+    def GetCoarseBox(self, i, fields):
+        # Get relevant parameters from header
+        t = self._coarse_box_headers[i,0]
+        ranks = int(self._coarse_box_headers[i,1])
+        dim0 = int(self._slice_headers[i,3])
+        downsample = int(self._slice_headers[i,4])
+
+        dim = int(dim0 / downsample)
+        folder = self._prefix + '/coarse_box/'
+
+        # Start dictionary
+        d = dict();
+        d['t'] = t
+
+        # Loop over fields, files, and boxes to reassemble data
+        for s in fields:
+            field = np.zeros((dim,dim,dim), dtype=np.float32)
+            for f in range(ranks):
+                file = folder + str(i) + '/' + str(f) + '.hdf5'
+
+                fin = h5py.File(file,'r')
+                if 'lex_data' in fin.keys():
+                    lx = np.array(fin['lex_data'], dtype='int')
+                    ly = np.array(fin['ley_data'], dtype='int')
+                    lz = np.array(fin['lez_data'], dtype='int')
+                    hx = np.array(fin['hex_data'], dtype='int')
+                    hy = np.array(fin['hey_data'], dtype='int')
+                    hz = np.array(fin['hez_data'], dtype='int')
+
+                    for b in range(len(lx)):
+                        dset = s+'_data_'+str(b+1)
+                        field[lx[b]:hx[b], ly[b]:hy[b], lz[b]:hz[b]] = \
+                                (fin[dset][:]).reshape(\
+                                        (hx[b]-lx[b], hy[b]-ly[b], hz[b]-lz[b]))
+
+            # Add result to dict.
+            d[s] = field
+
+        return d
+
+
     ## Determines what output exists.
     def __ParseFolderStructure(self):
         self.__ParseSlices()
+        self.__ParseCoarseLevelBoxes()
 
     ## Determines how many slices have been written and when.
     ## Reads header files.
     def __ParseSlices(self):
-        folder = self._prefix + '/slices'
+        folder = self._prefix + '/slices/'
         i = 0
 
         # iterate over batches and read header of first files
         while True:
-            file = folder + '/' + str(i) + '/Level_0/0.hdf5'
+            file = folder + str(i) + '/Level_0/0.hdf5'
             if not path.exists( file ):
                 break
             fin = h5py.File(file,'r')
             self._slice_headers.append( fin['Header_x'][:] )
             i = i + 1
-        
+
         self._slice_headers = np.array( self._slice_headers )
 
         print('Number of slices found: ', len(self._slice_headers))
-    
-    ## Path of output folder		
+
+    ## Determines how many coarse level boxes have been written and when.
+    ## Reads header files.
+    def __ParseCoarseLevelBoxes(self):
+        folder = self._prefix + '/coarse_box/'
+        i = 0
+
+        # iterate over batches and read header of first files
+        while True:
+            file = folder + str(i) + '/0.hdf5'
+            if not path.exists( file ):
+                break
+            fin = h5py.File(file,'r')
+            self._coarse_box_headers.append( fin['Header_data'][:] )
+            i = i + 1
+
+        self._coarse_box_headers = np.array( self._coarse_box_headers )
+
+        print('Number of coarse boxes found: ', len(self._coarse_box_headers))
+
+
+    ## Path of output folder
     _prefix = "."
 
     ## List of headers files from slices
     _slice_headers = []
+    _coarse_box_headers = []
