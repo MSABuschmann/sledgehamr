f6634c5cbad1faeac518899f19deb45374afc5a3
MSABuschman <buschman@umich.edu>
2023-10-10 05:04:54 -0700

    Temp debug statements. Performance monitor can cause stalls atm

--- source/performance_monitor.cpp
+++ source/performance_monitor.cpp
@@ -81,9 +81,6 @@ PerformanceMonitor::PerformanceMonitor(Sledgehamr* owner)
 }
 
 void PerformanceMonitor::Start(int id, int offset) {
-//    if (id == idx_fill_patch)
-//        amrex::AllPrint() << "perf: " << id << " " << offset << " " << active << " " << timer.size() << std::endl;
-
     if (active)
         timer[id + offset].Start();
 }
