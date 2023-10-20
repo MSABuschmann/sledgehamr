b04648f3201e14243fb8d0b66d0ae6f4e7909939
MSABuschman <buschman@umich.edu>
2023-10-20 12:15:31 -0700

    Create axion string prevolution class. EOM needs modification

---
+++ projects/axion_strings_preevolution/axion_strings_preevolution.h
@@ -0,0 +1,74 @@
+#ifndef PROJECTS_AXION_STRINGS_PREEVOLUTION_H_
+#define PROJECTS_AXION_STRINGS_PREEVOLUTION_H_
+
+#include <sledgehamr.h>
+#include <sledgehamr_utils.h>
+#include "../axion_strings/cosmology.h"
+
+namespace axion_strings_preevolution {
+using namespace axion_strings;
+
+/** @brief Function that calculates the RHS of the EOM at a single cell.
+ * @param   rhs     Container to be filled with RHS.
+ * @param   state   Data from which to calculate RHS (current state).
+ * @param   i       i-th cell index.
+ * @param   j       j-th cell index.
+ * @param   k       k-th cell index.
+ * @param   lev     Current level.
+ * @param   time    Current time.
+ * @param   dt      Time step size.
+ * @param   dx      Grid spacing.
+ */
+AMREX_GPU_DEVICE AMREX_FORCE_INLINE
+void Rhs(const amrex::Array4<double>& rhs,
+         const amrex::Array4<const double>& state,
+         const int i, const int j, const int k, const int lev,
+         const double time, const double dt, const double dx,
+         const double* params) {
+    // Fetch field values.
+    double Psi1 = state(i, j, k, Scalar::Psi1);
+    double Psi2 = state(i, j, k, Scalar::Psi2);
+    double Pi1  = state(i, j, k, Scalar::Pi1);
+    double Pi2  = state(i, j, k, Scalar::Pi2);
+
+    double eta = time;
+
+    // Compute Laplacians.
+    constexpr int order = 2;
+    double laplacian_Psi1 = sledgehamr::utils::Laplacian<order>(
+            state, i, j, k, Scalar::Psi1, dx*dx);
+    double laplacian_Psi2 = sledgehamr::utils::Laplacian<order>(
+            state, i, j, k, Scalar::Psi2, dx*dx);
+
+    // Compute EOM.
+    double potential = eta*eta*( Psi1*Psi1 + Psi2*Psi2 - 1. ) + 0.56233;
+
+    rhs(i, j, k, Scalar::Psi1) =  Pi1;
+    rhs(i, j, k, Scalar::Psi2) =  Pi2;
+    rhs(i, j, k, Scalar::Pi1)  = -Pi1*2./eta + laplacian_Psi1 - Psi1*potential;
+    rhs(i, j, k, Scalar::Pi2)  = -Pi2*2./eta + laplacian_Psi2 - Psi2*potential;
+}
+
+FINISH_SLEDGEHAMR_SETUP
+
+/** @brief Class to simulate axion strings.
+ */
+class axion_strings_preevolution : public sledgehamr::Sledgehamr {
+  public:
+    START_PROJECT(axion_strings_preevolution)
+
+    void Init() override {
+        cosmo.Init(this);
+    };
+
+    bool CreateLevelIf(const int lev, const double time) override {
+        return cosmo.CreateLevelIf(lev, time);
+    };
+
+  private:
+    Cosmology cosmo;
+};
+
+}; // namespace axion_strings_preevolution
+
+#endif // PROJECTS_AXION_STRINGS_PREEVOLUTION_H_
