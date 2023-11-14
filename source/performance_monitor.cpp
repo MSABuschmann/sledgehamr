dcbd9029f769f9bf993d6ac4beacead8e1fe3e16
MSABuschman <buschman@umich.edu>
2023-11-14 10:59:22 -0800

    Active performance monitor

--- source/performance_monitor.cpp
+++ source/performance_monitor.cpp
@@ -5,8 +5,8 @@ namespace sledgehamr {
 
 PerformanceMonitor::PerformanceMonitor(Sledgehamr* owner)
   : sim{owner} {
-    amrex::ParmParse pp("output");
-    pp.query("interval_performance_monitor", interval);
+    amrex::ParmParse pp("output.performance_monitor");
+    pp.query("interval", interval);
     active = (interval > 0);
 
     if (!active)
