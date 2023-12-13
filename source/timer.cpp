42c24b7ed2cfca5e16165939f59cdd7a58cf388f
MSABuschman <buschman@umich.edu>
2023-12-13 07:42:33 -0800

    More comments.

--- source/timer.cpp
+++ source/timer.cpp
@@ -2,6 +2,8 @@
 
 namespace sledgehamr {
 
+/** @brief Starts the timer by setting the start timer point.
+ */
 void Timer::Start() {
     if (is_running)
         return;
@@ -11,13 +13,8 @@ void Timer::Start() {
     is_running = true;
 }
 
-void Timer::CheckClock() {
-    stop_time = std::chrono::steady_clock::now();
-    last_duration_micro =
-            std::chrono::duration_cast<std::chrono::microseconds>(
-                stop_time - start_time);
-}
-
+/** @brief Stops the timer and checks time passed since it started running.
+ */
 void Timer::Stop() {
     if (!is_running)
         return;
@@ -28,6 +25,10 @@ void Timer::Stop() {
     is_running = false;
 }
 
+/** @brief Returns total time in seconds the timer was running across multiple
+ *         start/stop cycles.
+ *  return Time in seconds.
+ */
 double Timer::GetTotalTimeSeconds() {
     double extra_micro = 0;
     if (is_running) {
@@ -38,11 +39,24 @@ double Timer::GetTotalTimeSeconds() {
     return (total_micro + extra_micro)/1e6;
 };
 
+/** @brief Same as GetTotalTimeSeconds() but only time since the timer was 
+ *         last started.
+ */
 double Timer::GetLastDurationSeconds() {
     if (is_running)
         CheckClock();
 
     return static_cast<double>(last_duration_micro.count())/1e6;
 };
+
+/** @brief Computes time passed between now and the last starting time point
+           internally.
+ */
+void Timer::CheckClock() {
+    stop_time = std::chrono::steady_clock::now();
+    last_duration_micro =
+            std::chrono::duration_cast<std::chrono::microseconds>(
+                stop_time - start_time);
+}
 
 }; // namespace sledgehamr
--- source/timer.h
+++ source/timer.h
@@ -7,8 +7,13 @@
 
 namespace sledgehamr {
 
+/** @brief This class keeps track of passed time just like stopwatch.
+ */
 class Timer {
   public:
+    /** @brief Give the timer a display name during construction.
+     *  @param  my_name Display name of timner.
+     */
     Timer(std::string my_name) : name{my_name} {};
 
     void Start();
@@ -17,10 +22,14 @@ class Timer {
     double GetTotalTimeSeconds();
     double GetLastDurationSeconds();
 
+    /** @brief Returns display name of timer.
+     */
     std::string GetName() const {
         return name;
     };
 
+    /** @brief Returns whether the timer is currently running.
+     */
     bool IsRunning() const {
         return is_running;
     };
@@ -28,10 +37,30 @@ class Timer {
   private:
     void CheckClock();
 
-    std::chrono::steady_clock::time_point start_time, stop_time;
+    /** @brief Time point of when pointer has been started last.
+     */
+    std::chrono::steady_clock::time_point stop_time;
+
+    /** @brief Time point of when pointer has been stopped last.
+     */
+    std::chrono::steady_clock::time_point stop_time;
+
+    /** @brief Time in milliseconds from the last starting time point to when
+     *         Check() has been called last.
+     */
     std::chrono::microseconds last_duration_micro;
+
+    /** @brief Total time in milliseconds that the timer has been running
+     *         overall (across multiple start/stop cycles).
+     */
     double total_micro = 0;
+
+    /** @brief Display name of timer.
+     */
     const std::string name = "Unknown Timer";
+
+    /** @brief Flag whether timer is currently running.
+     */
     bool is_running = false;
 };
 
