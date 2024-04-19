8fe35e18a353df41fc110d55ce6e5ecfb1af6e4f
MSABuschman <buschman@umich.edu>
2024-04-19 09:14:07 -0700

    Quick fix to accomodate changes to GPU memory management defaults in amrex versions >23.08

--- source/main.cpp
+++ source/main.cpp
@@ -3,15 +3,19 @@
 #include <AMReX.H>
 #include <AMReX_BLProfiler.H>
 #include <AMReX_ParallelDescriptor.H>
-#include <AMReX_Print.H>
 #include <AMReX_ParmParse.H>
+#include <AMReX_Print.H>
 
 #include "sledgehamr.h"
 #include "sledgehamr_init.h"
 
-int main(int argc, char* argv[])
-{
-    amrex::Initialize(argc,argv);
+int main(int argc, char *argv[]) {
+    // Fix needed to accomodate change in amrex version 23.08. Will need long
+    // term solution for this.
+    amrex::ParmParse pp("amrex");
+    pp.add("the_arena_is_managed", 1);
+
+    amrex::Initialize(argc, argv);
 
     // Timer for profiling.
     BL_PROFILE_VAR("main()", pmain);
@@ -27,10 +31,10 @@ int main(int argc, char* argv[])
 
     // Print runtime.
     double end_total = amrex::ParallelDescriptor::second() - strt_total;
-    amrex::ParallelDescriptor::ReduceRealMax(end_total,
-            amrex::ParallelDescriptor::IOProcessorNumber());
-    amrex::Print() << std::endl << "Total Run Time: " << end_total << "s"
-                   << std::endl;
+    amrex::ParallelDescriptor::ReduceRealMax(
+        end_total, amrex::ParallelDescriptor::IOProcessorNumber());
+    amrex::Print() << std::endl
+                   << "Total Run Time: " << end_total << "s" << std::endl;
 
     // Destroy timer for profiling.
     BL_PROFILE_VAR_STOP(pmain);
