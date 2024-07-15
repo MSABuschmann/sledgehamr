3e661be7cd973976ce98ba5518df97930a7f75f4
MSABuschman <buschman@umich.edu>
2024-07-15 07:31:08 -0700

    Fix typo

--- projects/FirstOrderPhaseTransition/FirstOrderPhaseTransition.cpp
+++ projects/FirstOrderPhaseTransition/FirstOrderPhaseTransition.cpp
@@ -33,11 +33,12 @@ void FirstOrderPhaseTransition::ParseVariables() {
     pp_prj.query("potential_type", potential_type);
     switch (potential_type) {
     case PotentialType::PureLambdaBar:
-        amrex::Print("Running with pure \bar\lambda potential.");
+        amrex::Print() << "Running with pure \bar\lambda potential."
+                       << std::endl;
         pp_prj.get("lambda_bar", lambda_bar);
         break;
     case PotentialType::Piecewise:
-        amrex::Print("Running with piece-wise potential.");
+        amrex::Print() << "Running with piece-wise potential." << std::endl;
         pp_prj.get("lambda_bar", lambda_bar);
         pp_prj.get("vbar", vbar);
         pp_prj.get("vareps", vareps);
