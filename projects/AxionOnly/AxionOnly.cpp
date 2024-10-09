2b43f1b146d939fa9f13cad3fc86f8117feb689d
MSABuschman <buschman@umich.edu>
2024-10-09 08:26:53 -0700

    Change units

--- projects/AxionOnly/AxionOnly.cpp
+++ projects/AxionOnly/AxionOnly.cpp
@@ -11,6 +11,8 @@ void AxionOnly::ParseVariables() {
     amrex::ParmParse pp_prj("project");
     pp_prj.get("n", n);
     pp_prj.get("eta_c", eta_c);
+    pp_prj.get("eta_star", eta_star);
+    pp_prj.get("N_QCD", N_QCD);
 }
 
 void AxionOnly::SetProjections() {
@@ -20,7 +22,8 @@ void AxionOnly::SetProjections() {
 void AxionOnly::SetParamsRhs(std::vector<double> &params, const double time,
                              const int lev) {
     const double eta = time;
-    const double eta_growth = std::pow(std::min(eta, eta_c), n);
+    const double eta_growth =
+        std::pow(std::min(eta, eta_c) / eta_star, n) / N_QCD;
     params.push_back(eta_growth);
 }
 
