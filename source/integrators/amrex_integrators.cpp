4ea0f4c01362e08cee70d99133772210676a5296
MSABuschman <buschman@umich.edu>
2024-11-01 09:05:21 -0700

    Adapt AMReX RK integration scheme to be compatible with new AMReX versions

--- source/integrators/amrex_integrators.cpp
+++ source/integrators/amrex_integrators.cpp
@@ -9,25 +9,28 @@ namespace sledgehamr {
  * @param   mf_old  Current state.
  * @param   mf_new  New state after advancement.
  * @param   lev     Current level.
- * @param   dt      Time step size. 
+ * @param   dt      Time step size.
  * @param   dx      Grid spacing.
  */
-void IntegratorAMReX::Integrate(LevelData& mf_old, LevelData& mf_new,
-        const int lev, const double dt, const double dx) {
+void IntegratorAMReX::Integrate(LevelData &mf_old, LevelData &mf_new,
+                                const int lev, const double dt,
+                                const double dx) {
     // TODO Make 4th order time interpolation available.
     amrex::TimeIntegrator<amrex::MultiFab> integrator(mf_old);
 
-    auto source_fun = [&](amrex::MultiFab& rhs, const amrex::MultiFab& state,
+    auto source_fun = [&](amrex::MultiFab &rhs, amrex::MultiFab &state,
                           const double time) {
+        sim->level_synchronizer->FillIntermediatePatch(lev, time, state);
         sim->FillRhs(rhs, state, time, lev, dt, dx);
     };
 
-    auto post_update_fun = [&](amrex::MultiFab& S_data, const double time) {
-        sim->level_synchronizer->FillIntermediatePatch(lev, time, S_data);
-    };
+    //   auto post_update_fun = [&](amrex::MultiFab& S_data, const double time)
+    //   {
+    //       sim->level_synchronizer->FillIntermediatePatch(lev, time, S_data);
+    //   };
 
     integrator.set_rhs(source_fun);
-    integrator.set_post_update(post_update_fun);
+    //    integrator.set_post_update(post_update_fun);
     integrator.advance(mf_old, mf_new, mf_old.t, dt);
 }
 
